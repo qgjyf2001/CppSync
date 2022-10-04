d77f6582da065c7a337c9b2c58751bba37c2aba1
qgjyf2001 <qgjyf2001@163.com>
2022-10-04 17:32:04 +0800

    update

--- benchmark.cpp
+++ benchmark.cpp
@@ -1,6 +1,7 @@
 #include <coroutine>
 #include <iostream>
 #include <future>
+#include <cxxabi.h>
 
 #include "./include/graph.h"
 
@@ -49,6 +50,44 @@ public:
     }
 };
 
+decltype(std::chrono::steady_clock::now()) start;
+
+template <typename graphType>
+void testGraph(std::shared_ptr<graph> graph_,std::shared_ptr<Node> final) {
+    char buffer[64]={0};
+    size_t length=64;
+    __cxxabiv1::__cxa_demangle(typeid(graphType).name(),buffer,&length,nullptr);
+    std::string clazz=buffer;
+    std::cout<<"=======testing class "+ clazz + "======="<<std::endl;
+    // build run graph
+    auto runGraph=std::make_shared<graphType>();
+    using nodeType=std::remove_reference<decltype(*runGraph)>::type::Node;
+    std::unordered_map<std::shared_ptr<Node>,std::shared_ptr<nodeType>> nodeMap;
+    int number=0;
+    for (auto &node:graph_->nodes) {
+        auto graphNode=runGraph->add_node(node->func)->name("node"+std::to_string(number++));
+        nodeMap[node]=graphNode;
+    }
+    for (auto &node:graph_->nodes) {
+        for (auto &depend:node->depends) {
+            nodeMap[node]->depend(nodeMap[depend]);
+        }
+    }
+
+    std::cout<<"graph build done"<<std::endl;
+    //run dag
+    auto &finalNode=nodeMap[final];
+    start=std::chrono::steady_clock::now();
+
+    if constexpr(std::is_same<graphType,cppSync::graph>::value) {
+        (*finalNode)().get_promise()->get_future().get();
+    } else if constexpr(std::is_same<graphType,cppSync::noCoroutine::graph>::value) {
+        runGraph->run_graph();
+        finalNode->get_promise()->get_future().get();
+    } else {
+        static_assert("typename not supported");
+    }
+}
 int main() {
     auto graph_=std::make_shared<graph>();
     const int nodeSize=10000;
@@ -62,32 +101,15 @@ int main() {
             }
         });
     }
-    decltype(std::chrono::steady_clock::now()) start;  
-    auto final=graph_->genDepends([&start,&count]() {
+    auto final=graph_->genDepends([&count]() {
         auto end=std::chrono::steady_clock::now();
         std::chrono::duration<double,std::milli> duration=end-start;
         int result=count;
         std::cout<<"graph execution done, count:"<<result<<", timeout:"<<duration.count()<<"ms"<<std::endl;
     });
 
-    // build async graph
-    auto asyncGraph=std::make_shared<cppSync::graph>();
-    std::unordered_map<std::shared_ptr<Node>,std::shared_ptr<cppSync::graph::Node>> nodeMap;
-    int number=0;
-    for (auto &node:graph_->nodes) {
-        auto graphNode=asyncGraph->add_node(node->func)->name("node"+std::to_string(number++));
-        nodeMap[node]=graphNode;
-    }
-    for (auto &node:graph_->nodes) {
-        for (auto &depend:node->depends) {
-            nodeMap[node]->depend(nodeMap[depend]);
-        }
-    }
+    testGraph<cppSync::graph>(graph_,final);
+    testGraph<cppSync::noCoroutine::graph>(graph_,final);
 
-    std::cout<<"graph build done"<<std::endl;
-    //run dag
-    auto &finalNode=nodeMap[final];
-    start=std::chrono::steady_clock::now();
-    (*finalNode)().get_promise()->get_future().get();
     return 0;
 }
--- include/graph.h
+++ include/graph.h
@@ -2,11 +2,81 @@
 #define GRAPH_H
 
 #include <functional>
+#include <unordered_set>
 #include <map>
 #include "sync.h"
 #include "syncTask.h"
 
 namespace cppSync {
+
+namespace noCoroutine {
+    class graph {
+public:
+    class Node;
+    class Node : public std::enable_shared_from_this<Node> {
+        private:
+            std::vector<std::shared_ptr<Node>> preceed;
+            std::function<void()> func;
+            graph* graph_;
+            std::atomic<int> depends;
+            std::shared_ptr<promise<void>> promise_=std::make_shared<promise<void>>();
+        public:
+        std::string name_;
+        Node(graph* graph_,std::function<void()> func,std::string name="unknown"):graph_(graph_),func(func),name_(name) {
+            graph_->startNodes.insert(this);
+            graph_->runNodes++;
+        }
+        auto name(std::string name_) {
+            this->name_ = name_;
+            return shared_from_this();
+        }
+        void depend(std::shared_ptr<Node> node) {
+            node->preceed.push_back(shared_from_this());
+            depends++;
+            if (graph_->startNodes.find(this)!=graph_->startNodes.end()) {
+                graph_->startNodes.erase(this);
+            }
+        }
+        auto get_promise() {
+            return promise_;
+        }
+        void operator()() {
+            func();
+            promise_->set_value();
+            int nodeLeft=--graph_->runNodes;
+            if (nodeLeft==0) {
+                graph_->promise_->set_value();
+            }
+            for (auto &node:preceed) {
+                int depend_=--node->depends;
+                if (depend_==0) {
+                    threadPool::instance<THREADPOOL_SIZE>().addThread([node](){
+                        (*node)();
+                    });
+                }
+            }
+            return;
+        }
+    };
+private:
+    std::unordered_set<Node*> startNodes;
+    std::atomic<int> runNodes;
+    std::shared_ptr<promise<void>> promise_=std::make_shared<promise<void>>();
+public:
+    template <typename F,typename... Args>
+    std::shared_ptr<Node> add_node(F f,Args... args) {
+        return std::make_shared<Node>(this,std::bind(f,args...));
+    }
+    auto run_graph() {
+        for (auto &node:startNodes) {
+            threadPool::instance<THREADPOOL_SIZE>().addThread([node](){
+                        (*node)();
+            });
+        }
+        return promise_;
+    }
+};
+}
 
 class graph {
 public:
@@ -15,7 +85,6 @@ public:
         private:
             std::vector<std::shared_ptr<Node>> depends;
             std::function<void()> func;
-            std::condition_variable consumer;
             graph* graph_;
         public:
         std::string name_;
